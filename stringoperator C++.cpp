988f6d15c4ff624cdaf5e295151c7c1204d5911d
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-18 19:22:01 +0530

    Add files via upload

---
+++ stringoperator C++.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string a;
+    string b;
+    string c;
+    //int a;
+    //int b;
+    //int c;
+    //getline(cin,a);
+
+    cout<<"The strings which you have given as input are: "<<a<<b<<endl;
+    c=a+b;
+    cout<<"The combined word is: "<<c<<endl;
+    cout<<"The length of the combined name is: "<<c.length();
+    return 0;
+
+}
